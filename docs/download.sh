19d672ff8c3f0565755717cde7339bf7a835e342
Kayce Basques <kaycebasques@gmail.com>
2026-08-21 20:23:15 +0000

    cleanup

--- docs/download.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-curl -L -o pico1.pdf https://pip-assets.raspberrypi.com/categories/610-raspberry-pi-pico/documents/RP-008307-DS-2-pico-datasheet.pdf
-curl -L -o pico2.pdf https://pip-assets.raspberrypi.com/categories/1005-raspberry-pi-pico-2/documents/RP-008299-DS-3-pico-2-datasheet.pdf
-curl -L -o rp2040.pdf https://pip-assets.raspberrypi.com/categories/814-rp2040/documents/RP-008371-DS-1-rp2040-datasheet.pdf
-curl -L -o rp2350.pdf https://pip-assets.raspberrypi.com/categories/1214-rp2350/documents/RP-008373-DS-2-rp2350-datasheet.pdf
